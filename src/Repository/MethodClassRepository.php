70585ebed63fea4a751f74046ff3e6fcccf1fe73
Elites2017 <daveluv09@gmail.com>
2022-03-03 14:12:42 +0100

    Create MethodClass entity & repository

---
+++ src/Repository/MethodClassRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\MethodClass;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method MethodClass|null find($id, $lockMode = null, $lockVersion = null)
+ * @method MethodClass|null findOneBy(array $criteria, array $orderBy = null)
+ * @method MethodClass[]    findAll()
+ * @method MethodClass[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MethodClassRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, MethodClass::class);
+    }
+
+    // /**
+    //  * @return MethodClass[] Returns an array of MethodClass objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('m')
+            ->andWhere('m.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('m.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?MethodClass
+    {
+        return $this->createQueryBuilder('m')
+            ->andWhere('m.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
