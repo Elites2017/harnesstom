7ecf7ff0ce834e230dc92e381067ed16bbb340ef
Elites2017 <daveluv09@gmail.com>
2022-03-03 14:21:58 +0100

    Create VarCallSoftware entity & repository

---
+++ src/Repository/VarCallSoftwareRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\VarCallSoftware;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method VarCallSoftware|null find($id, $lockMode = null, $lockVersion = null)
+ * @method VarCallSoftware|null findOneBy(array $criteria, array $orderBy = null)
+ * @method VarCallSoftware[]    findAll()
+ * @method VarCallSoftware[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class VarCallSoftwareRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, VarCallSoftware::class);
+    }
+
+    // /**
+    //  * @return VarCallSoftware[] Returns an array of VarCallSoftware objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('v')
+            ->andWhere('v.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('v.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?VarCallSoftware
+    {
+        return $this->createQueryBuilder('v')
+            ->andWhere('v.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
