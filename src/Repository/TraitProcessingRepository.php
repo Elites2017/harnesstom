3ca0a511ab08173b7a29ba1494cf777fbbe5b094
Elites2017 <daveluv09@gmail.com>
2022-03-03 14:19:24 +0100

    Create TraitProcessing entity & repository

---
+++ src/Repository/TraitProcessingRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\TraitProcessing;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method TraitProcessing|null find($id, $lockMode = null, $lockVersion = null)
+ * @method TraitProcessing|null findOneBy(array $criteria, array $orderBy = null)
+ * @method TraitProcessing[]    findAll()
+ * @method TraitProcessing[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TraitProcessingRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, TraitProcessing::class);
+    }
+
+    // /**
+    //  * @return TraitProcessing[] Returns an array of TraitProcessing objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('t.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?TraitProcessing
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
