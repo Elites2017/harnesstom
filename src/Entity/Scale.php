a1c914069abd58ed0fe33b508d9a47f43a575e74
Elites2017 <daveluv09@gmail.com>
2022-03-03 16:10:09 +0100

    Create Scale entity & repository

---
+++ src/Entity/Scale.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ScaleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ScaleRepository::class)
+ */
+class Scale
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=ScaleCategory::class, inversedBy="scales")
+     */
+    private $scaleCategory;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=DataType::class, inversedBy="scales")
+     */
+    private $dataType;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Unit::class, inversedBy="scales")
+     */
+    private $unit;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="scales")
+     */
+    private $createdBy;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getScaleCategory(): ?ScaleCategory
+    {
+        return $this->scaleCategory;
+    }
+
+    public function setScaleCategory(?ScaleCategory $scaleCategory): self
+    {
+        $this->scaleCategory = $scaleCategory;
+
+        return $this;
+    }
+
+    public function getDataType(): ?DataType
+    {
+        return $this->dataType;
+    }
+
+    public function setDataType(?DataType $dataType): self
+    {
+        $this->dataType = $dataType;
+
+        return $this;
+    }
+
+    public function getUnit(): ?Unit
+    {
+        return $this->unit;
+    }
+
+    public function setUnit(?Unit $unit): self
+    {
+        $this->unit = $unit;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?User
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy(?User $createdBy): self
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+}
