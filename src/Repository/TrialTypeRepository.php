7fdf07efae8a9ec91fc8947b5b202b419338df19
Elites2017 <daveluv09@gmail.com>
2022-03-03 14:20:27 +0100

    Create TrialType entity & repository

---
+++ src/Repository/TrialTypeRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\TrialType;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method TrialType|null find($id, $lockMode = null, $lockVersion = null)
+ * @method TrialType|null findOneBy(array $criteria, array $orderBy = null)
+ * @method TrialType[]    findAll()
+ * @method TrialType[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TrialTypeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, TrialType::class);
+    }
+
+    // /**
+    //  * @return TrialType[] Returns an array of TrialType objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('t.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?TrialType
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
