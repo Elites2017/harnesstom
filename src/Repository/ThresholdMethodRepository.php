61d851a1933609deb9f38088c2064287cdc8938f
Elites2017 <daveluv09@gmail.com>
2022-03-03 14:18:41 +0100

    Create ThresholdMethod entity & repository

---
+++ src/Repository/ThresholdMethodRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ThresholdMethod;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method ThresholdMethod|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ThresholdMethod|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ThresholdMethod[]    findAll()
+ * @method ThresholdMethod[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ThresholdMethodRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ThresholdMethod::class);
+    }
+
+    // /**
+    //  * @return ThresholdMethod[] Returns an array of ThresholdMethod objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('t.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?ThresholdMethod
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
