dbad666d5282792c997a18aaf3a5af2cbe1682c7
Elites2017 <daveluv09@gmail.com>
2022-03-03 14:05:35 +0100

    Create CiCriteria entity & repository

---
+++ src/Repository/CiCriteriaRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\CiCriteria;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method CiCriteria|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CiCriteria|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CiCriteria[]    findAll()
+ * @method CiCriteria[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CiCriteriaRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, CiCriteria::class);
+    }
+
+    // /**
+    //  * @return CiCriteria[] Returns an array of CiCriteria objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?CiCriteria
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
