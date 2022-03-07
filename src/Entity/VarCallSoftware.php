ef4d9723041bdc6abb71bce72ea49dd15596b0a5
Elites2017 <daveluv09@gmail.com>
2022-03-07 12:35:50 +0100

    Add VarCallSoftware / GenotypingPlaform relationship

--- src/Entity/VarCallSoftware.php
+++ src/Entity/VarCallSoftware.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\VarCallSoftwareRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -36,6 +38,16 @@ class VarCallSoftware
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="varCallSoftware")
      */
     private $createdBy;
+
+    /**
+     * @ORM\OneToMany(targetEntity=GenotypingPlatform::class, mappedBy="varCallSoftware")
+     */
+    private $genotypingPlatforms;
+
+    public function __construct()
+    {
+        $this->genotypingPlatforms = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -86,6 +98,36 @@ class VarCallSoftware
     public function setCreatedBy(?User $createdBy): self
     {
         $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, GenotypingPlatform>
+     */
+    public function getGenotypingPlatforms(): Collection
+    {
+        return $this->genotypingPlatforms;
+    }
+
+    public function addGenotypingPlatform(GenotypingPlatform $genotypingPlatform): self
+    {
+        if (!$this->genotypingPlatforms->contains($genotypingPlatform)) {
+            $this->genotypingPlatforms[] = $genotypingPlatform;
+            $genotypingPlatform->setVarCallSoftware($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGenotypingPlatform(GenotypingPlatform $genotypingPlatform): self
+    {
+        if ($this->genotypingPlatforms->removeElement($genotypingPlatform)) {
+            // set the owning side to null (unless already changed)
+            if ($genotypingPlatform->getVarCallSoftware() === $this) {
+                $genotypingPlatform->setVarCallSoftware(null);
+            }
+        }
 
         return $this;
     }
