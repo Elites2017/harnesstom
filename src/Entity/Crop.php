f0864b68992acb24e9ab2c106a1f888c59e329a6
Elites2017 <daveluv09@gmail.com>
2022-03-07 14:48:16 +0100

    Add Crop / Program relationship

--- src/Entity/Crop.php
+++ src/Entity/Crop.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CropRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -36,6 +38,16 @@ class Crop
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="crops")
      */
     private $createdBy;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Program::class, mappedBy="crop")
+     */
+    private $programs;
+
+    public function __construct()
+    {
+        $this->programs = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -86,6 +98,36 @@ class Crop
     public function setCreatedBy(?User $createdBy): self
     {
         $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Program>
+     */
+    public function getPrograms(): Collection
+    {
+        return $this->programs;
+    }
+
+    public function addProgram(Program $program): self
+    {
+        if (!$this->programs->contains($program)) {
+            $this->programs[] = $program;
+            $program->setCrop($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProgram(Program $program): self
+    {
+        if ($this->programs->removeElement($program)) {
+            // set the owning side to null (unless already changed)
+            if ($program->getCrop() === $this) {
+                $program->setCrop(null);
+            }
+        }
 
         return $this;
     }
