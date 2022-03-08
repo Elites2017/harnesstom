fbaa2fc687ad8da742d8083f5fe8cb17b25839e8
Elites2017 <daveluv09@gmail.com>
2022-03-08 16:03:38 +0100

    Create SharedWith entity & repository

---
+++ src/Repository/SharedWithRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\SharedWith;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method SharedWith|null find($id, $lockMode = null, $lockVersion = null)
+ * @method SharedWith|null findOneBy(array $criteria, array $orderBy = null)
+ * @method SharedWith[]    findAll()
+ * @method SharedWith[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SharedWithRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, SharedWith::class);
+    }
+
+    // /**
+    //  * @return SharedWith[] Returns an array of SharedWith objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?SharedWith
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
