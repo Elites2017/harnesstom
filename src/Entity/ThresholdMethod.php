24710ae60ecb99797d7309c07c478845bfbcc014
David <daveluv09@gmail.com>
2022-03-09 17:04:38 +0100

    Add Threshold / GWAS createdBy relationship

--- src/Entity/ThresholdMethod.php
+++ src/Entity/ThresholdMethod.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ThresholdMethodRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -46,6 +48,16 @@ class ThresholdMethod
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="thresholdMethods")
      */
     private $createdBy;
+
+    /**
+     * @ORM\OneToMany(targetEntity=GWAS::class, mappedBy="thresholdMethod")
+     */
+    private $gWAS;
+
+    public function __construct()
+    {
+        $this->gWAS = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -120,6 +132,36 @@ class ThresholdMethod
     public function setCreatedBy(?User $createdBy): self
     {
         $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, GWAS>
+     */
+    public function getGWAS(): Collection
+    {
+        return $this->gWAS;
+    }
+
+    public function addGWA(GWAS $gWA): self
+    {
+        if (!$this->gWAS->contains($gWA)) {
+            $this->gWAS[] = $gWA;
+            $gWA->setThresholdMethod($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGWA(GWAS $gWA): self
+    {
+        if ($this->gWAS->removeElement($gWA)) {
+            // set the owning side to null (unless already changed)
+            if ($gWA->getThresholdMethod() === $this) {
+                $gWA->setThresholdMethod(null);
+            }
+        }
 
         return $this;
     }
