f7a4b07c87cc86ee44fffcc6a95b1aaf0aaf86a0
David <daveluv09@gmail.com>
2022-03-09 17:07:42 +0100

    Add AllelicEffectEstimator t / GWAS  relationship

--- src/Entity/AllelicEffectEstimator.php
+++ src/Entity/AllelicEffectEstimator.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\AllelicEffectEstimatorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -36,6 +38,16 @@ class AllelicEffectEstimator
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="allelicEffectEstimators")
      */
     private $createdBy;
+
+    /**
+     * @ORM\OneToMany(targetEntity=GWAS::class, mappedBy="allelicEffectEstimator")
+     */
+    private $gWAS;
+
+    public function __construct()
+    {
+        $this->gWAS = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -86,6 +98,36 @@ class AllelicEffectEstimator
     public function setCreatedBy(?User $createdBy): self
     {
         $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, GWAS>
+     */
+    public function getGWAS(): Collection
+    {
+        return $this->gWAS;
+    }
+
+    public function addGWA(GWAS $gWA): self
+    {
+        if (!$this->gWAS->contains($gWA)) {
+            $this->gWAS[] = $gWA;
+            $gWA->setAllelicEffectEstimator($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGWA(GWAS $gWA): self
+    {
+        if ($this->gWAS->removeElement($gWA)) {
+            // set the owning side to null (unless already changed)
+            if ($gWA->getAllelicEffectEstimator() === $this) {
+                $gWA->setAllelicEffectEstimator(null);
+            }
+        }
 
         return $this;
     }
