eac7dabc3a0a7c3b37790604c38af16a13f6ddb6
David <daveluv09@gmail.com>
2022-03-09 17:06:37 +0100

    Add GWASSTatTest / GWAS  relationship

--- src/Entity/GWASStatTest.php
+++ src/Entity/GWASStatTest.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GWASStatTestRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -46,6 +48,16 @@ class GWASStatTest
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="gWASStatTests")
      */
     private $createdBy;
+
+    /**
+     * @ORM\OneToMany(targetEntity=GWAS::class, mappedBy="gwasStatTest")
+     */
+    private $gWAS;
+
+    public function __construct()
+    {
+        $this->gWAS = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -120,6 +132,36 @@ class GWASStatTest
     public function setCreatedBy(?User $createdBy): self
     {
         $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, GWAS>
+     */
+    public function getGWAS(): Collection
+    {
+        return $this->gWAS;
+    }
+
+    public function addGWA(GWAS $gWA): self
+    {
+        if (!$this->gWAS->contains($gWA)) {
+            $this->gWAS[] = $gWA;
+            $gWA->setGwasStatTest($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGWA(GWAS $gWA): self
+    {
+        if ($this->gWAS->removeElement($gWA)) {
+            // set the owning side to null (unless already changed)
+            if ($gWA->getGwasStatTest() === $this) {
+                $gWA->setGwasStatTest(null);
+            }
+        }
 
         return $this;
     }
