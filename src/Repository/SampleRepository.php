362471a9508a4ee605ca676f31dcb4ee8903e853
Elites2017 <daveluv09@gmail.com>
2022-03-10 10:33:50 +0100

    Create Sample entity & repository

---
+++ src/Repository/SampleRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Sample;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Sample|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Sample|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Sample[]    findAll()
+ * @method Sample[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SampleRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Sample::class);
+    }
+
+    // /**
+    //  * @return Sample[] Returns an array of Sample objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Sample
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
