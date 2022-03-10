ea7cdb487b61cacdb8a17c55d7c3342851fa198c
Elites2017 <daveluv09@gmail.com>
2022-03-10 13:44:09 +0100

    Add QTLMethod / QTLStudy relationship

--- src/Entity/QTLMethod.php
+++ src/Entity/QTLMethod.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\QTLMethodRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -46,6 +48,16 @@ class QTLMethod
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="qTLMethods")
      */
     private $createdBy;
+
+    /**
+     * @ORM\OneToMany(targetEntity=QTLStudy::class, mappedBy="method")
+     */
+    private $qTLStudies;
+
+    public function __construct()
+    {
+        $this->qTLStudies = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -120,6 +132,36 @@ class QTLMethod
     public function setCreatedBy(?User $createdBy): self
     {
         $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, QTLStudy>
+     */
+    public function getQTLStudies(): Collection
+    {
+        return $this->qTLStudies;
+    }
+
+    public function addQTLStudy(QTLStudy $qTLStudy): self
+    {
+        if (!$this->qTLStudies->contains($qTLStudy)) {
+            $this->qTLStudies[] = $qTLStudy;
+            $qTLStudy->setMethod($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQTLStudy(QTLStudy $qTLStudy): self
+    {
+        if ($this->qTLStudies->removeElement($qTLStudy)) {
+            // set the owning side to null (unless already changed)
+            if ($qTLStudy->getMethod() === $this) {
+                $qTLStudy->setMethod(null);
+            }
+        }
 
         return $this;
     }
