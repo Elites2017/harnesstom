ec631e013b5889360eea25f1c8a83a6ba94b52b5
Elites2017 <daveluv09@gmail.com>
2022-03-10 12:30:32 +0100

    Create MetaboliteValue entiry & repository

---
+++ src/Entity/MetaboliteValue.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MetaboliteValueRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MetaboliteValueRepository::class)
+ */
+class MetaboliteValue
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $value;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $createdBy;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Sample::class, inversedBy="metaboliteValues")
+     */
+    private $sample;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Metabolite::class, inversedBy="metaboliteValues")
+     */
+    private $metabolite;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getValue(): ?float
+    {
+        return $this->value;
+    }
+
+    public function setValue(float $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?string
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy(?string $createdBy): self
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    public function getSample(): ?Sample
+    {
+        return $this->sample;
+    }
+
+    public function setSample(?Sample $sample): self
+    {
+        $this->sample = $sample;
+
+        return $this;
+    }
+
+    public function getMetabolite(): ?Metabolite
+    {
+        return $this->metabolite;
+    }
+
+    public function setMetabolite(?Metabolite $metabolite): self
+    {
+        $this->metabolite = $metabolite;
+
+        return $this;
+    }
+}
