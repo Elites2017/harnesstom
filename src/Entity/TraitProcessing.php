bf1dedce8d276cb1f3360299c502fa2969f6af9a
Elites2017 <daveluv09@gmail.com>
2022-03-10 11:55:16 +0100

    Add TraitPreprocessing /GWASVariant relationship

--- src/Entity/TraitProcessing.php
+++ src/Entity/TraitProcessing.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\TraitProcessingRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -41,6 +43,16 @@ class TraitProcessing
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="traitProcessings")
      */
     private $createdBy;
+
+    /**
+     * @ORM\OneToMany(targetEntity=GWASVariant::class, mappedBy="traitPreprocessing")
+     */
+    private $gWASVariants;
+
+    public function __construct()
+    {
+        $this->gWASVariants = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -103,6 +115,36 @@ class TraitProcessing
     public function setCreatedBy(?User $createdBy): self
     {
         $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, GWASVariant>
+     */
+    public function getGWASVariants(): Collection
+    {
+        return $this->gWASVariants;
+    }
+
+    public function addGWASVariant(GWASVariant $gWASVariant): self
+    {
+        if (!$this->gWASVariants->contains($gWASVariant)) {
+            $this->gWASVariants[] = $gWASVariant;
+            $gWASVariant->setTraitPreprocessing($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGWASVariant(GWASVariant $gWASVariant): self
+    {
+        if ($this->gWASVariants->removeElement($gWASVariant)) {
+            // set the owning side to null (unless already changed)
+            if ($gWASVariant->getTraitPreprocessing() === $this) {
+                $gWASVariant->setTraitPreprocessing(null);
+            }
+        }
 
         return $this;
     }
