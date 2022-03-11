9bd4ef52d05418cf8755d35df512042ee48123d8
Elites2017 <daveluv09@gmail.com>
2022-03-11 14:24:43 +0100

    Create the form to create the season

---
+++ src/Form/SeasonType.php
@@ -0,0 +1,30 @@
+<?php
+/* 
+    This class is to be called in the SeasonController to create the seasonForm.
+    March 11, 2022
+    David PIERRE
+*/
+
+namespace App\Form;
+
+use App\Entity\Season;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeasonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description');
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Season::class,
+        ]);
+    }
+}
