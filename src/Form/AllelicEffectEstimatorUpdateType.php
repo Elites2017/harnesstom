2d59490db925485a6ff3abfdf8fa76e86a4dcf60
Elites2017 <daveluv09@gmail.com>
2022-03-14 14:08:37 +0100

    Create AllelicEffectEstimator edition form

---
+++ src/Form/AllelicEffectEstimatorUpdateType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\AllelicEffectEstimator;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AllelicEffectEstimatorUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('createdAt')
+            ->add('isActive')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AllelicEffectEstimator::class,
+        ]);
+    }
+}
