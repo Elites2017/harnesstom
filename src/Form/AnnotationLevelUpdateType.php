6b4008951ffc96990e4582e5ea0b10f44c0d5c24
Elites2017 <daveluv09@gmail.com>
2022-03-14 15:45:08 +0100

    Create AnnotationLevel edition form

---
+++ src/Form/AnnotationLevelUpdateType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\AnnotationLevel;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnotationLevelUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('label')
+            ->add('code')
+            ->add('createdAt')
+            ->add('isActive')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AnnotationLevel::class,
+        ]);
+    }
+}
