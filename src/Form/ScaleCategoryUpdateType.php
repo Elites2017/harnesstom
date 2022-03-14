e55808f99e749e577c8ac09a0bc5e2166e1ac814
Elites2017 <daveluv09@gmail.com>
2022-03-14 12:12:10 +0100

    Create ScaleCategory edit form

---
+++ src/Form/ScaleCategoryUpdateType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ScaleCategory;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ScaleCategoryUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('label')
+            ->add('score')
+            ->add('min')
+            ->add('max')
+            ->add('createdAt')
+            ->add('isActive')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ScaleCategory::class,
+        ]);
+    }
+}
