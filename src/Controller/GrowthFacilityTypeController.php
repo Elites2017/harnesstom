4d1e9e1dda9c9c53912c647d1ab5a4d878165ef4
Elites2017 <daveluv09@gmail.com>
2022-03-15 15:06:17 +0100

    Create the GrowthFailityType controller

---
+++ src/Controller/GrowthFacilityTypeController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\GrowthFacilityType;
+use App\Form\GrowthFacilityCreateType;
+use App\Form\GrowthFacilityUpdateType;
+use App\Repository\GrowthFacilityTypeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+ // set a class level route
+ /**
+ * @Route("growth/facility/type", name="growth_facility_type_")
+ */
+class GrowthFacilityTypeController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(GrowthFacilityTypeRepository $growthFacilityTypeRepo): Response
+    {
+        $growthFacilityTypes =  $growthFacilityTypeRepo->findAll();
+        $context = [
+            'title' => 'Growth Facility Type List',
+            'growthFacilityTypes' => $growthFacilityTypes
+        ];
+        return $this->render('growth_facility_type/index.html.twig', $context);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create(Request $request, EntityManagerInterface $entmanager): Response
+    {
+        $growthFacilityType = new GrowthFacilityType();
+        $form = $this->createForm(GrowthFacilityCreateType::class, $growthFacilityType);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $growthFacilityType->setIsActive(true);
+            $growthFacilityType->setCreatedAt(new \DateTime());
+            $entmanager->persist($growthFacilityType);
+            $entmanager->flush();
+            return $this->redirect($this->generateUrl('growth_facility_type_index'));
+        }
+
+        $context = [
+            'title' => 'Growth Facility Type Creation',
+            'growthFacilityTypeForm' => $form->createView()
+        ];
+        return $this->render('growth_facility_type/create.html.twig', $context);
+    }
+
+    /**
+     * @Route("/details/{id}", name="details")
+     */
+    public function details(GrowthFacilityType $growthFacilityTypeSelected): Response
+    {
+        $context = [
+            'title' => 'Growth Facility Type Details',
+            'growthFacilityType' => $growthFacilityTypeSelected
+        ];
+        return $this->render('growth_facility_type/details.html.twig', $context);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     */
+    public function edit(GrowthFacilityType $growthFacilityType, Request $request, EntityManagerInterface $entmanager): Response
+    {
+        $form = $this->createForm(GrowthFacilityUpdateType::class, $growthFacilityType);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entmanager->persist($growthFacilityType);
+            $entmanager->flush();
+            return $this->redirect($this->generateUrl('growth_facility_type_index'));
+        }
+
+        $context = [
+            'title' => 'Growth Facility Type Update',
+            'growthFacilityTypeForm' => $form->createView()
+        ];
+        return $this->render('growth_facility_type/edit.html.twig', $context);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(GrowthFacilityType $growthFacilityType, EntityManagerInterface $entmanager): Response
+    {
+        if ($growthFacilityType->getId()) {
+            $growthFacilityType->setIsActive(!$growthFacilityType->getIsActive());
+        }
+        $entmanager->persist($growthFacilityType);
+        $entmanager->flush();
+
+        return $this->json([
+            'code' => 200,
+            'message' => $growthFacilityType->getIsActive()
+        ], 200);
+        //return $this->redirect($this->generateUrl('growthFacility_home'));
+    }
+}
