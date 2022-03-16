2ed6a05055bac9e26f5e0e9b6bd398c4fdd07f04
David <daveluv09@gmail.com>
2022-03-16 10:36:00 +0100

    Create MLSStatus creation form

---
+++ src/Form/MLSStatusType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\MLSStatus;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MLSStatusType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('label')
+            ->add('code')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => MLSStatus::class,
+        ]);
+    }
+}
