f91c56c2266b8b4b59d1d235b85433535720c537
David <daveluv09@gmail.com>
2022-03-16 15:36:35 +0100

    Create TraitClass controller

---
+++ src/Controller/TraitClassController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\TraitClass;
+use App\Form\TraitClassType;
+use App\Form\TraitClassUpdateType;
+use App\Repository\TraitClassRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+// set a class level route
+/**
+ * @Route("/trait", name="trait_class_")
+ */
+class TraitClassController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(TraitClassRepository $traitClassRepo): Response
+    {
+        $traitClasses =  $traitClassRepo->findAll();
+        $context = [
+            'title' => 'Trait List',
+            'traitClasses' => $traitClasses
+        ];
+        return $this->render('trait_class/index.html.twig', $context);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create(Request $request, EntityManagerInterface $entmanager): Response
+    {
+        $traitClass = new TraitClass();
+        $form = $this->createForm(TraitClassType::class, $traitClass);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $traitClass->setIsActive(true);
+            $traitClass->setCreatedAt(new \DateTime());
+            $entmanager->persist($traitClass);
+            $entmanager->flush();
+            return $this->redirect($this->generateUrl('trait_class_index'));
+        }
+
+        $context = [
+            'title' => 'Trait Creation',
+            'traitClassForm' => $form->createView()
+        ];
+        return $this->render('trait_class/create.html.twig', $context);
+    }
+
+    /**
+     * @Route("/details/{id}", name="details")
+     */
+    public function details(TraitClass $traitClasseSelected): Response
+    {
+        $context = [
+            'title' => 'Trait Details',
+            'traitClass' => $traitClasseSelected
+        ];
+        return $this->render('trait_class/details.html.twig', $context);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     */
+    public function edit(TraitClass $traitClass, Request $request, EntityManagerInterface $entmanager): Response
+    {
+        $form = $this->createForm(TraitClassUpdateType::class, $traitClass);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entmanager->persist($traitClass);
+            $entmanager->flush();
+            return $this->redirect($this->generateUrl('trait_class_index'));
+        }
+
+        $context = [
+            'title' => 'Trait Update',
+            'traitClassForm' => $form->createView()
+        ];
+        return $this->render('trait_class/edit.html.twig', $context);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(TraitClass $traitClass, Request $request, EntityManagerInterface $entmanager): Response
+    {
+        if ($traitClass->getId()) {
+            $traitClass->setIsActive(!$traitClass->getIsActive());
+        }
+        $entmanager->persist($traitClass);
+        $entmanager->flush();
+
+        return $this->json([
+            'code' => 200,
+            'message' => $traitClass->getIsActive()
+        ], 200);
+        //return $this->redirect($this->generateUrl('season_home'));
+    }
+}
