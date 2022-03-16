ce1c3f7c808301bab06e99293f73beda32df2021
David <daveluv09@gmail.com>
2022-03-16 15:12:40 +0100

    Create Taxonomy creation form

---
+++ src/Form/TaxonomyType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Taxonomy;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaxonomyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('taxonid')
+            ->add('genus')
+            ->add('species')
+            ->add('subtaxa')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Taxonomy::class,
+        ]);
+    }
+}
