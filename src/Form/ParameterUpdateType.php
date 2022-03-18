0eb4351ae8ccd56c9b54589c6ce313cf78c4d08c
Elites2017 <daveluv09@gmail.com>
2022-03-18 14:56:16 +0100

    Create Parameter edition form

---
+++ src/Form/ParameterUpdateType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Parameter;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParameterUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('isActive')
+            ->add('factorType')
+            ->add('unit')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Parameter::class,
+        ]);
+    }
+}
