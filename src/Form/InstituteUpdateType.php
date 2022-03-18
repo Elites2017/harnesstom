b32790cfa90f2b229b9ce9280339f2b47aebaf4f
Elites2017 <daveluv09@gmail.com>
2022-03-18 11:11:31 +0100

    Create Institute edition form

---
+++ src/Form/InstituteUpdateType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Institute;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InstituteUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('instcode')
+            ->add('acronym')
+            ->add('name')
+            ->add('streetNumber')
+            ->add('postalCode')
+            ->add('city')
+            ->add('isActive')
+            ->add('country')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Institute::class,
+        ]);
+    }
+}
