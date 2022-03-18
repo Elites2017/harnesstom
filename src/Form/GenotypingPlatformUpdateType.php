5d95b8c81b41242aa10803e74f29d039b6bc9def
Elites2017 <daveluv09@gmail.com>
2022-03-18 13:59:17 +0100

    Create GenotypingPlatform edition form

---
+++ src/Form/GenotypingPlatformUpdateType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\GenotypingPlatform;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GenotypingPlatformUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('methodDescription')
+            ->add('refSetName')
+            ->add('publishedDate')
+            ->add('bioProjectID')
+            ->add('markerCount')
+            ->add('assemblyPUI')
+            ->add('publicationRef')
+            ->add('isActive')
+            ->add('sequencingType')
+            ->add('sequencingInstrument')
+            ->add('varCallSoftware')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => GenotypingPlatform::class,
+        ]);
+    }
+}
