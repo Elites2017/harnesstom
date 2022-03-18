698b1f753d6f37903e816058a152ca9c45047d5f
Elites2017 <daveluv09@gmail.com>
2022-03-18 16:01:19 +0100

    Create Contact edition form

---
+++ src/Form/ContactUpdateType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('orcid')
+            ->add('isActive')
+            ->add('person')
+            ->add('institute')
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Program Leader' => 'Program Leader',
+                    'Coordinator' => 'Coordinator',
+                    'Adviser' => 'Adviser',
+                    'Submitter' => 'Submitter'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
