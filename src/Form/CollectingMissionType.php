1c60f5e4aa245f5c7d841fca20b221219734cd9c
Elites2017 <daveluv09@gmail.com>
2022-03-21 10:22:32 +0100

    Create CollectingMission creation form

---
+++ src/Form/CollectingMissionType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CollectingMission;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CollectingMissionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('species')
+            ->add('institute')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => CollectingMission::class,
+        ]);
+    }
+}
