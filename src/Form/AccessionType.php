a787cada7973485d57c5fa7834b3d797f55196e5
Elites2017 <daveluv09@gmail.com>
2022-03-22 16:39:32 +0100

    Create Accession creation form

---
+++ src/Form/AccessionType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Accession;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AccessionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('accenumb')
+            ->add('accename')
+            ->add('puid')
+            ->add('origmuni')
+            ->add('origadmin1')
+            ->add('origadmin2')
+            ->add('maintainernumb')
+            ->add('acqdate')
+            ->add('donornumb')
+            ->add('collnumb')
+            ->add('colldate')
+            ->add('declatitude')
+            ->add('declongitude')
+            ->add('elevation')
+            ->add('collsite')
+            ->add('origcty')
+            ->add('collsrc')
+            ->add('sampstat')
+            ->add('taxon')
+            ->add('instcode')
+            ->add('storage')
+            ->add('donorcode')
+            ->add('collcode')
+            ->add('collmissid')
+            ->add('bredcode')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Accession::class,
+        ]);
+    }
+}
