7c8463f6e06304c3f75e5eccebfab3c57f988614
Elites2017 <daveluv09@gmail.com>
2022-03-22 15:32:16 +0100

    Create Trial creation form

---
+++ src/Form/TrialType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trial;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrialType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('abbreviation')
+            ->add('startDate')
+            ->add('endDate')
+            ->add('publicReleaseDate')
+            ->add('license')
+            ->add('pui')
+            ->add('publicationReference')
+            ->add('program')
+            ->add('trialType')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Trial::class,
+        ]);
+    }
+}
