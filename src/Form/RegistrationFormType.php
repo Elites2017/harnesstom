76528a8ec0baf64309783482bd67539439917b90
Elites2017 <daveluv09@gmail.com>
2022-04-11 13:43:38 +0200

    Add repeated password

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use App\Form\PersonType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -27,11 +28,11 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, [
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -42,7 +43,10 @@ class RegistrationFormType extends AbstractType
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
-                ],
+                ],],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password'],
             ])
             ->add('person', PersonType::class)
         ;
