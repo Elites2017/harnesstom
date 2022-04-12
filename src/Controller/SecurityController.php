7c17f3015c3281472287747354d67b97046e87b6
Elites2017 <daveluv09@gmail.com>
2022-04-12 16:26:35 +0200

    Add change password function

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -25,6 +30,37 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/change_password", name="change_password")
+     */
+    public function changePassword(Request $request, UserPasswordHasherInterface $passwordEncoder, EntityManagerInterface $entmanager)
+    {
+        // DP on April 12th 2022
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $form = $this->createForm(ChangePasswordFormType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $old_pwd = $form->get('oldPassword')->getData();
+            $user = $this->getUser();
+            $checkPassword = $passwordEncoder->isPasswordValid($user, $old_pwd);
+            if ($checkPassword) {
+                $new_pwd = $form->get('plainPassword')->getData();
+                $user->setPassword($passwordEncoder->hashPassword($user, $new_pwd));
+                $entmanager->persist($user);
+                $entmanager->flush();
+                $this->addFlash('password_successfully_updated', "Your password has been successfuly updated");
+                return $this->redirect($this->generateUrl('app_home'));
+            } else {
+                $this->addFlash('old_password_error', "The password you typed doesn't match your old password");
+            }
+        }
+        $context = [
+            'title' => 'Change Password',
+            'changePasswordForm' => $form->createView()
+        ];
+        return $this->render('change_password/change_password.html.twig', $context);
     }
 
     /**
