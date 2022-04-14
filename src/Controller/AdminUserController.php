50aed48ed83b374f9ae35cb82f349932d6287b85
Elites2017 <daveluv09@gmail.com>
2022-04-14 17:39:31 +0200

    Add adm user controller

---
+++ src/Controller/AdminUserController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\PersonRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+// set a class level route
+/**
+ * @Route("/admin/user", name="admin_user_")
+ */
+class AdminUserController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(PersonRepository $personRepo): Response
+    {
+        $persons = $personRepo->findAll();
+        //dd($persons);
+        $context = [
+            'title' => 'Admin User',
+            'persons' => $persons
+        ];
+        return $this->render('admin_user/index.html.twig', $context);
+    }
+
+    /**
+     * @Route("/details/{id}", name="details")
+     */
+    public function details(User $userSelected): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $context = [
+            'title' => 'Admin User Details',
+            'user' => $userSelected
+        ];
+        return $this->render('admin_user/details.html.twig', $context);
+    }
+
+    /**
+     * @Route("/change_role/{id}", name="change_role")
+     */
+    public function changeRole(User $user, EntityManagerInterface $entmanager): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        if ($user->getId()) {
+            $roles = $user->getRoles();
+            $myV = false;
+            $ind = "";
+            // search for the ROLE_ADMIN
+            foreach (array_keys($roles, 'ROLE_ADMIN') as $key) {
+                $myV = true;
+                //unset($array[$key]);
+               $ind = $key;
+            }
+            if ($myV) {
+                unset($roles[$ind]);
+                $roles = array_values($roles);
+                
+            } else {
+                $roles [] = "ROLE_ADMIN";
+            }
+            
+            $user->setRoles($roles);
+        }
+        $entmanager->persist($user);
+        $entmanager->flush();
+
+        return $this->json([
+            'code' => 200,
+            'ccc' => $myV,
+            'message' => $myV ? ' Make Admin ' : 'Remove Admin'
+        ], 200);
+        //return $this->redirect($this->generateUrl('season_home'));
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function changeStatus(User $user, EntityManagerInterface $entmanager): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        if ($user->getId()) {
+            $user->setIsActive(!$user->getIsActive());
+        }
+        $entmanager->persist($user);
+        $entmanager->flush();
+
+        return $this->json([
+            'code' => 200,
+            'message' => $user->getIsActive()
+        ], 200);
+        //return $this->redirect($this->generateUrl('season_home'));
+    }
+}
