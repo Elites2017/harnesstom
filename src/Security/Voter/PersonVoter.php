a84f322be731f915c52fed8b0e083224861c2276
David <daveluv09@gmail.com>
2022-04-15 16:03:39 +0200

    Add person voter

---
+++ src/Security/Voter/PersonVoter.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Person;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Security;
+
+class PersonVoter extends Voter
+{
+    public const PERSON_EDIT = 'person_edit';
+    public const PERSON_DEL = 'person_del';
+
+    // to get the user roles as suggested by symfony
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    protected function supports(string $attribute, $person): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::PERSON_EDIT, self::PERSON_DEL])
+            && $person instanceof \App\Entity\Person;
+    }
+
+    protected function voteOnAttribute(string $attribute, $person, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // check if user is admin
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return true;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case self::PERSON_EDIT:
+                // logic to determine if the user can EDIT
+                // return true or false
+                return $this->canEdit($person, $user);
+                break;
+            case self::PERSON_DEL:
+                // logic to determine if the user can VIEW
+                // return true or false
+                return $this->canDelete($person, $user);
+                break;
+        }
+
+        return false;
+    }
+
+    // private methods
+    private function canEdit(Person $person, User $user){
+        // if the connected user is the created of the person object, they can modify it
+        return $user === $person->getCreatedBy();
+    }
+
+    private function canDelete(Person $person, User $user){
+        // if the connected user is the created of the person object, they can modify it
+        return $user === $person->getCreatedBy();  
+    }
+}
