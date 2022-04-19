dc3029d4c06ca7bd527d13dede85e67065fe0ed5
Elites2017 <daveluv09@gmail.com>
2022-04-19 16:29:19 +0200

    Create Study edition form

---
+++ src/Form/StudyUpdateType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Study;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StudyUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('abbreviation')
+            ->add('description')
+            ->add('startDate', DateType::class, array(
+                'widget' => 'single_text',
+            ))
+            ->add('endDate', DateType::class, array(
+                'widget' => 'single_text',
+            ))
+            ->add('culturalPractice')
+            ->add('isActive')
+            ->add('trial')
+            ->add('factor')
+            ->add('season')
+            ->add('institute')
+            ->add('location')
+            ->add('growthFacility')
+            ->add('parameter')
+            ->add('experimentalDesignType')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Study::class,
+        ]);
+    }
+}
