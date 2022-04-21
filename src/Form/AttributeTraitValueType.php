58b5ffc1f0c8fefe29cfb22f069981055e492e53
Elites2017 <daveluv09@gmail.com>
2022-04-21 16:01:17 +0200

    Create AttributeTraitValue creation form

---
+++ src/Form/AttributeTraitValueType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\AttributeTraitValue;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AttributeTraitValueType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('value')
+            ->add('publicationReference')
+            ->add('trait')
+            ->add('metabolicTrait')
+            ->add('attribute')
+            ->add('accession')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AttributeTraitValue::class,
+        ]);
+    }
+}
