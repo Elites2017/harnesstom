b9cbe491bd09d46a4cd29f7825d8cbec99ff2637
Elites2017 <daveluv09@gmail.com>
2022-05-02 15:44:01 +0200

    Create GWAS edition form studyID list to be fixed

---
+++ src/Form/GWASType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\GWAS;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GWASType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('preprocessing')
+            ->add('thresholdValue')
+            ->add('studyID', CollectionType::class, [
+                'entry_type' => TextType::class,
+                'allow_add' => true,
+                'prototype' => true,
+                'label' => false,
+                'prototype_data' => 'Study...'
+            ])
+            ->add('publicationReference', CollectionType::class, [
+                'entry_type' => TextType::class,
+                'allow_add' => true,
+                'prototype' => true,
+                'label' => false,
+                'prototype_data' => 'Publication reference...'
+            ])
+            ->add('variantSetMetada')
+            ->add('software')
+            ->add('gwasModel')
+            ->add('kinshipAlgorithm')
+            ->add('structureMethod')
+            ->add('geneticTestingModel')
+            ->add('allelicEffectEstimator')
+            ->add('gwasStatTest')
+            ->add('thresholdMethod')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => GWAS::class,
+        ]);
+    }
+}
