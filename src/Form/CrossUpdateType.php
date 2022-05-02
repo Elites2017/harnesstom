f5fd2e3f3335a9405808bde5440724adac22b139
Elites2017 <daveluv09@gmail.com>
2022-05-02 10:12:29 +0200

    Create Cross edition form

---
+++ src/Form/CrossUpdateType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Cross;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CrossUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description', TextareaType::class, [
+                'attr' => array('cols' => '5', 'rows' => '7')])
+            ->add('parent1Type')
+            ->add('parent2Type')
+            ->add('year')
+            ->add('publicationReference')
+            ->add('study')
+            ->add('institute')
+            ->add('breedingMethod')
+            ->add('parent1')
+            ->add('parent2')
+            ->add('pedigree')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Cross::class,
+        ]);
+    }
+}
