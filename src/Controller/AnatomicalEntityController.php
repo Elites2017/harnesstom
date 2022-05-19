9612155f99f01b139baf1d0704ee03a42cd2d975
Elites2017 <daveluv09@gmail.com>
2022-05-19 15:39:58 +0200

    Anatomical Entity Upload From Excel File

--- src/Controller/AnatomicalEntityController.php
+++ src/Controller/AnatomicalEntityController.php
@@ -5,11 +5,15 @@ namespace App\Controller;
 use App\Entity\AnatomicalEntity;
 use App\Form\AnatomicalEntityType;
 use App\Form\AnatomicalEntityUpdateType;
+use App\Form\UploadFromExcelType;
 use App\Repository\AnatomicalEntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 // set a class level route
@@ -109,5 +113,118 @@ class AnatomicalEntityController extends AbstractController
             'message' => $anatomicalEntity->getIsActive()
         ], 200);
         //return $this->redirect($this->generateUrl('factorType_home'));
+    }
+
+    // this is to upload data in bulk using an excel file
+    /**
+     * @Route("/upload-from-excel", name="upload_from_excel")
+     */
+    public function uploadFromExcel(Request $request, EntityManagerInterface $entmanager): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $form = $this->createForm(UploadFromExcelType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Setup repository of some entity
+            $repoAnatomicalEntity = $entmanager->getRepository(AnatomicalEntity::class);
+            // Query how many rows are there in the AnatomicalEntity table
+            $totalAnatomicalEntityBefore = $repoAnatomicalEntity->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            // Return a number as response
+            // e.g 972
+
+            // get the file (name from the CountryUploadFromExcelType form)
+            $file = $request->files->get('upload_from_excel')['file'];
+            // set the folder to send the file to
+            $fileFolder = __DIR__ . '/../../public/uploads/excel/';
+            // apply md5 function to generate a unique id for the file and concat it with the original file name
+            if ($file->getClientOriginalName()) {
+                $filePathName = md5(uniqid()) . $file->getClientOriginalName();
+                try {
+                    $file->move($fileFolder, $filePathName);
+                } catch (\Throwable $th) {
+                    //throw $th;
+                    $this->addFlash('danger', "Fail to upload the file, try again");
+                }
+            } else {
+                $this->addFlash('danger', "Error in the file name, try to rename the file and try again");
+            }
+            // read from the uploaded file
+            $spreadsheet = IOFactory::load($fileFolder . $filePathName);
+            // remove the first row (title) of the file
+            $spreadsheet->getActiveSheet()->removeRow(1);
+            // transform the uploaded file to an array
+            $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+            // loop over the array to get each row
+            foreach ($sheetData as $key => $row) {
+                $name = $row['A'];
+                $description = $row['B'];
+                $ontology_id = $row['C'];
+                $parentTerm = $row['D'];
+                // check if the file doesn't have empty columns
+                if ($name != null && $description != null && $ontology_id != null && $parentTerm != null) {
+                    // check if the data is upload in the database
+                    $existingAnatomicalEntity = $entmanager->getRepository(AnatomicalEntity::class)->findOneBy(['name' => $name]);
+                    // upload data only for countries that haven't been saved in the database
+                    if (!$existingAnatomicalEntity) {
+                        $anatomicalEntity = new AnatomicalEntity();
+                        if ($this->getUser()) {
+                            $anatomicalEntity->setCreatedBy($this->getUser());
+                        }
+                        $anatomicalEntity->setName($name);
+                        $anatomicalEntity->setDescription($description);
+                        $anatomicalEntity->setOntologyId($ontology_id);
+                        $anatomicalEntity->setDescription($parentTerm);
+                        $anatomicalEntity->setIsActive(true);
+                        $anatomicalEntity->setCreatedAt(new \DateTime());
+                        $entmanager->persist($anatomicalEntity);
+                    }
+                }
+            }
+            $entmanager->flush();
+            // Query how many rows are there in the Country table
+            $totalAnatomicalEntityAfter = $repoAnatomicalEntity->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            if ($totalAnatomicalEntityBefore == 0) {
+                $this->addFlash('success', $totalAnatomicalEntityAfter . " anatomical entities have been successfuly added");
+            } else {
+                $diffBeforeAndAfter = $totalAnatomicalEntityAfter - $totalAnatomicalEntityBefore;
+                if ($diffBeforeAndAfter == 0) {
+                    $this->addFlash('success', "No new anatomical entity has been added");
+                } else if ($diffBeforeAndAfter == 1) {
+                    $this->addFlash('success', $diffBeforeAndAfter . " anatomical entity has been successfuly added");
+                } else {
+                    $this->addFlash('success', $diffBeforeAndAfter . " anatomical entities have been successfuly added");
+                }
+            }
+            return $this->redirect($this->generateUrl('factor_type_index'));
+        }
+
+        $context = [
+            'title' => 'Anatomical Entity Upload From Excel',
+            'anatomicalEntityUploadFromExcelForm' => $form->createView()
+        ];
+        return $this->render('anatomical_entity/upload_from_excel.html.twig', $context);
+    }
+
+    /**
+     * @Route("/download-template", name="download_template")
+     */
+    public function factorTypeTemplate(): Response
+    {
+        $response = new BinaryFileResponse('../public/todownload/anatomical_entity_template_example.xls');
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'anatomical_entity_template_example.xls');
+        return $response;
+       
     }
 }
