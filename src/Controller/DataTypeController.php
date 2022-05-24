4e658097c80a41cfc6c26946ca5990c3ace036ce
Elites2017 <daveluv09@gmail.com>
2022-05-24 11:17:00 +0200

    Data Type Upload From Excel

--- src/Controller/DataTypeController.php
+++ src/Controller/DataTypeController.php
@@ -5,11 +5,15 @@ namespace App\Controller;
 use App\Entity\DataType;
 use App\Form\DataCreateType;
 use App\Form\DataUpdateType;
+use App\Form\UploadFromExcelType;
 use App\Repository\DataTypeRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 // set a class level route
@@ -109,5 +113,112 @@ class DataTypeController extends AbstractController
             'message' => $dataType->getIsActive()
         ], 200);
         //return $this->redirect($this->generateUrl('season_home'));
+    }
+
+    // this is to upload data in bulk using an excel file
+    /**
+     * @Route("/upload-from-excel", name="upload_from_excel")
+     */
+    public function uploadFromExcel(Request $request, EntityManagerInterface $entmanager): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $form = $this->createForm(UploadFromExcelType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Setup repository of some entity
+            $repoDataType = $entmanager->getRepository(DataType::class);
+            // Query how many rows are there in the DataType table
+            $totalDataTypeBefore = $repoDataType->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            // Return a number as response
+            // e.g 972
+
+            // get the file (name from the CountryUploadFromExcelType form)
+            $file = $request->files->get('upload_from_excel')['file'];
+            // set the folder to send the file to
+            $fileFolder = __DIR__ . '/../../public/uploads/excel/';
+            // apply md5 function to generate a unique id for the file and concat it with the original file name
+            if ($file->getClientOriginalName()) {
+                $filePathName = md5(uniqid()) . $file->getClientOriginalName();
+                try {
+                    $file->move($fileFolder, $filePathName);
+                } catch (\Throwable $th) {
+                    //throw $th;
+                    $this->addFlash('danger', "Fail to upload the file, try again");
+                }
+            } else {
+                $this->addFlash('danger', "Error in the file name, try to rename the file and try again");
+            }
+            // read from the uploaded file
+            $spreadsheet = IOFactory::load($fileFolder . $filePathName);
+            // remove the first row (title) of the file
+            $spreadsheet->getActiveSheet()->removeRow(1);
+            // transform the uploaded file to an array
+            $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+            // loop over the array to get each row
+            foreach ($sheetData as $key => $row) {
+                $label = $row['A'];
+                // check if the file doesn't have empty columns
+                if ($label != null) {
+                    // check if the data is upload in the database
+                    $existingDataType = $entmanager->getRepository(DataType::class)->findOneBy(['label' => $label]);
+                    // upload data only for countries that haven't been saved in the database
+                    if (!$existingDataType) {
+                        $DataType = new DataType();
+                        if ($this->getUser()) {
+                            $DataType->setCreatedBy($this->getUser());
+                        }
+                        $DataType->setLabel($label);
+                        $DataType->setIsActive(true);
+                        $DataType->setCreatedAt(new \DateTime());
+                        $entmanager->persist($DataType);
+                    }
+                }
+            }
+            $entmanager->flush();
+            // Query how many rows are there in the Country table
+            $totalDataTypeAfter = $repoDataType->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            if ($totalDataTypeBefore == 0) {
+                $this->addFlash('success', $totalDataTypeAfter . " data types have been successfuly added");
+            } else {
+                $diffBeforeAndAfter = $totalDataTypeAfter - $totalDataTypeBefore;
+                if ($diffBeforeAndAfter == 0) {
+                    $this->addFlash('success', "No new data type has been added");
+                } else if ($diffBeforeAndAfter == 1) {
+                    $this->addFlash('success', $diffBeforeAndAfter . " data type has been successfuly added");
+                } else {
+                    $this->addFlash('success', $diffBeforeAndAfter . " data types have been successfuly added");
+                }
+            }
+            return $this->redirect($this->generateUrl('data_type_index'));
+        }
+
+        $context = [
+            'title' => 'DataType Upload From Excel',
+            'dataTypeUploadFromExcelForm' => $form->createView()
+        ];
+        return $this->render('data_type/upload_from_excel.html.twig', $context);
+    }
+
+    /**
+     * @Route("/download-template", name="download_template")
+     */
+    public function factorTypeTemplate(): Response
+    {
+        $response = new BinaryFileResponse('../public/todownload/data_type_template_example.xls');
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'data_type_template_example.xls');
+        return $response;
+       
     }
 }
