bbf063c05408aee761e74ce7905fb0787be32dcf
Elites2017 <daveluv09@gmail.com>
2022-05-24 10:43:12 +0200

    Ci Criteria Upload From Ecel

--- src/Controller/CiCriteriaController.php
+++ src/Controller/CiCriteriaController.php
@@ -5,11 +5,15 @@ namespace App\Controller;
 use App\Entity\CiCriteria;
 use App\Form\CiCriteriaType;
 use App\Form\CiCriteriaUpdateType;
+use App\Form\UploadFromExcelType;
 use App\Repository\CiCriteriaRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 // set a class level route
@@ -109,5 +113,116 @@ class CiCriteriaController extends AbstractController
             'message' => $ciCriteria->getIsActive()
         ], 200);
         //return $this->redirect($this->generateUrl('ciCriteria_home'));
+    }
+
+    // this is to upload data in bulk using an excel file
+    /**
+     * @Route("/upload-from-excel", name="upload_from_excel")
+     */
+    public function uploadFromExcel(Request $request, EntityManagerInterface $entmanager): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $form = $this->createForm(UploadFromExcelType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Setup repository of some entity
+            $repoCiCriteria = $entmanager->getRepository(CiCriteria::class);
+            // Query how many rows are there in the CiCriteria table
+            $totalCiCriteriaBefore = $repoCiCriteria->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            // Return a number as response
+            // e.g 972
+
+            // get the file (name from the CountryUploadFromExcelType form)
+            $file = $request->files->get('upload_from_excel')['file'];
+            // set the folder to send the file to
+            $fileFolder = __DIR__ . '/../../public/uploads/excel/';
+            // apply md5 function to generate a unique id for the file and concat it with the original file name
+            if ($file->getClientOriginalName()) {
+                $filePathName = md5(uniqid()) . $file->getClientOriginalName();
+                try {
+                    $file->move($fileFolder, $filePathName);
+                } catch (\Throwable $th) {
+                    //throw $th;
+                    $this->addFlash('danger', "Fail to upload the file, try again");
+                }
+            } else {
+                $this->addFlash('danger', "Error in the file name, try to rename the file and try again");
+            }
+            // read from the uploaded file
+            $spreadsheet = IOFactory::load($fileFolder . $filePathName);
+            // remove the first row (title) of the file
+            $spreadsheet->getActiveSheet()->removeRow(1);
+            // transform the uploaded file to an array
+            $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+            // loop over the array to get each row
+            foreach ($sheetData as $key => $row) {
+                $ontology_id = $row['A'];
+                $name = $row['B'];
+                $parentTerm = $row['C'];
+                // check if the file doesn't have empty columns
+                if ($ontology_id != null && $name != null) {
+                    // check if the data is upload in the database
+                    $existingCiCriteria = $entmanager->getRepository(CiCriteria::class)->findOneBy(['ontology_id' => $ontology_id]);
+                    // upload data only for countries that haven't been saved in the database
+                    if (!$existingCiCriteria) {
+                        $ciCriteria = new CiCriteria();
+                        if ($this->getUser()) {
+                            $ciCriteria->setCreatedBy($this->getUser());
+                        }
+                        $ciCriteria->setOntologyId($ontology_id);
+                        $ciCriteria->setName($name);
+                        $ciCriteria->setParentTerm($parentTerm);
+                        $ciCriteria->setIsActive(true);
+                        $ciCriteria->setCreatedAt(new \DateTime());
+                        $entmanager->persist($ciCriteria);
+                    }
+                }
+            }
+            $entmanager->flush();
+            // Query how many rows are there in the Country table
+            $totalCiCriteriaAfter = $repoCiCriteria->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            if ($totalCiCriteriaBefore == 0) {
+                $this->addFlash('success', $totalCiCriteriaAfter . " ci criterias have been successfuly added");
+            } else {
+                $diffBeforeAndAfter = $totalCiCriteriaAfter - $totalCiCriteriaBefore;
+                if ($diffBeforeAndAfter == 0) {
+                    $this->addFlash('success', "No new ci criteria has been added");
+                } else if ($diffBeforeAndAfter == 1) {
+                    $this->addFlash('success', $diffBeforeAndAfter . " ci criteria has been successfuly added");
+                } else {
+                    $this->addFlash('success', $diffBeforeAndAfter . " ci criterias have been successfuly added");
+                }
+            }
+            return $this->redirect($this->generateUrl('ci_criteria_index'));
+        }
+
+        $context = [
+            'title' => 'Ci Criteria Upload From Excel',
+            'ciCriteriaUploadFromExcelForm' => $form->createView()
+        ];
+        return $this->render('ci_criteria/upload_from_excel.html.twig', $context);
+    }
+
+    /**
+     * @Route("/download-template", name="download_template")
+     */
+    public function factorTypeTemplate(): Response
+    {
+        $response = new BinaryFileResponse('../public/todownload/ci_criteria_template_example.xls');
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'ci_criteria_template_example.xls');
+        return $response;
+       
     }
 }
