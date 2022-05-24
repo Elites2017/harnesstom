e5f2fb593ff31c0998dea090223f749da4abf403
Elites2017 <daveluv09@gmail.com>
2022-05-24 13:35:14 +0200

    GWAS Stat Test Upload From Excel File

--- src/Controller/GWASStatTestController.php
+++ src/Controller/GWASStatTestController.php
@@ -5,11 +5,15 @@ namespace App\Controller;
 use App\Entity\GWASStatTest;
 use App\Form\GWASStatTestType;
 use App\Form\GWASStatTestUpdateType;
+use App\Form\UploadFromExcelType;
 use App\Repository\GWASStatTestRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
  // set a class level route
@@ -109,5 +113,114 @@ class GWASStatTestController extends AbstractController
             'message' => $gwasStatTest->getIsActive()
         ], 200);
         //return $this->redirect($this->generateUrl('growthFacility_home'));
+    }
+
+    // this is to upload data in bulk using an excel file
+    /**
+     * @Route("/upload-from-excel", name="upload_from_excel")
+     */
+    public function uploadFromExcel(Request $request, EntityManagerInterface $entmanager): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $form = $this->createForm(UploadFromExcelType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Setup repository of some entity
+            $repoGWASStatTest = $entmanager->getRepository(GWASStatTest::class);
+            // Query how many rows are there in the GWASStatTest table
+            $totalGWASStatTestBefore = $repoGWASStatTest->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            // Return a number as response
+            // e.g 972
+
+            // get the file (name from the CountryUploadFromExcelType form)
+            $file = $request->files->get('upload_from_excel')['file'];
+            // set the folder to send the file to
+            $fileFolder = __DIR__ . '/../../public/uploads/excel/';
+            // apply md5 function to generate a unique id for the file and concat it with the original file name
+            if ($file->getClientOriginalName()) {
+                $filePathName = md5(uniqid()) . $file->getClientOriginalName();
+                try {
+                    $file->move($fileFolder, $filePathName);
+                } catch (\Throwable $th) {
+                    //throw $th;
+                    $this->addFlash('danger', "Fail to upload the file, try again");
+                }
+            } else {
+                $this->addFlash('danger', "Error in the file name, try to rename the file and try again");
+            }
+            // read from the uploaded file
+            $spreadsheet = IOFactory::load($fileFolder . $filePathName);
+            // remove the first row (title) of the file
+            $spreadsheet->getActiveSheet()->removeRow(1);
+            // transform the uploaded file to an array
+            $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+            // loop over the array to get each row
+            foreach ($sheetData as $key => $row) {
+                $name = $row['A'];
+                $ontology_id = $row['B'];
+                // check if the file doesn't have empty columns
+                if ($name != null & $ontology_id != null) {
+                    // check if the data is upload in the database
+                    $existingGWASStatTest = $entmanager->getRepository(GWASStatTest::class)->findOneBy(['name' => $name]);
+                    // upload data only for countries that haven't been saved in the database
+                    if (!$existingGWASStatTest) {
+                        $GWASStatTest = new GWASStatTest();
+                        if ($this->getUser()) {
+                            $GWASStatTest->setCreatedBy($this->getUser());
+                        }
+                        $GWASStatTest->setName($name);
+                        $GWASStatTest->setOntologyId($ontology_id);
+                        $GWASStatTest->setIsActive(true);
+                        $GWASStatTest->setCreatedAt(new \DateTime());
+                        $entmanager->persist($GWASStatTest);
+                    }
+                }
+            }
+            $entmanager->flush();
+            // Query how many rows are there in the Country table
+            $totalGWASStatTestAfter = $repoGWASStatTest->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            if ($totalGWASStatTestBefore == 0) {
+                $this->addFlash('success', $totalGWASStatTestAfter . " gwas stat tests have been successfuly added");
+            } else {
+                $diffBeforeAndAfter = $totalGWASStatTestAfter - $totalGWASStatTestBefore;
+                if ($diffBeforeAndAfter == 0) {
+                    $this->addFlash('success', "No new gwas stat test has been added");
+                } else if ($diffBeforeAndAfter == 1) {
+                    $this->addFlash('success', $diffBeforeAndAfter . " gwas stat test has been successfuly added");
+                } else {
+                    $this->addFlash('success', $diffBeforeAndAfter . " gwas stat tests have been successfuly added");
+                }
+            }
+            return $this->redirect($this->generateUrl('gwas_stat_test_index'));
+        }
+
+        $context = [
+            'title' => 'GWAS Stat Test Upload From Excel',
+            'gWASStatTestUploadFromExcelForm' => $form->createView()
+        ];
+        return $this->render('gwas_stat_test/upload_from_excel.html.twig', $context);
+    }
+
+    /**
+     * @Route("/download-template", name="download_template")
+     */
+    public function factorTypeTemplate(): Response
+    {
+        $response = new BinaryFileResponse('../public/todownload/gwas_stat_test_template_example.xls');
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'gwas_stat_test_template_example.xls');
+        return $response;
+       
     }
 }
