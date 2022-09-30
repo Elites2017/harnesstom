802fa85a14bb4221c9c6f3f0acb9501646c7b964
Elites2017 <daveluv09@gmail.com>
2022-09-30 13:24:20 +0200

    Add new entity link creation for updating institute

--- src/Form/InstituteUpdateType.php
+++ src/Form/InstituteUpdateType.php
@@ -2,15 +2,25 @@
 
 namespace App\Form;
 
+use App\Entity\Country;
 use App\Entity\Institute;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\RouterInterface;
 
 class InstituteUpdateType extends AbstractType
 {
+    private $router;
+
+    function __construct(RouterInterface $router){
+        $this->router = $router;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $tosUrl = $this->router->generate('country_create');
         $builder
             ->add('instcode')
             ->add('acronym')
@@ -18,7 +28,13 @@ class InstituteUpdateType extends AbstractType
             ->add('streetNumber')
             ->add('postalCode')
             ->add('city')
-            ->add('country')
+            ->add('country', EntityType::class, [
+                'class' => Country::class,
+                'help_html' => true,
+                'placeholder' => '',
+                'help' => 'Add a new <a href="' . $tosUrl .'" target="_blank">Country</a>'
+                
+            ])
         ;
     }
 
