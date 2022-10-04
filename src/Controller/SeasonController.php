d371f2db2f703cb8a45cc5596e2328c1814522a0
Elites2017 <daveluv09@gmail.com>
2022-10-04 15:27:19 +0200

    Update Season Controller

--- src/Controller/SeasonController.php
+++ src/Controller/SeasonController.php
@@ -136,4 +136,13 @@ class SeasonController extends AbstractController
         ], 200);
         //return $this->redirect($this->generateUrl('season_home'));
     }
+
+    // this is to upload data in bulk using an excel file
+    /**
+     * @Route("/upload-from-excel", name="upload_from_excel")
+     */
+    public function uploadFromExcel(Request $request, EntityManagerInterface $entmanager): Response
+    {
+        dd("Good morning SEASON");
+    }
 }
