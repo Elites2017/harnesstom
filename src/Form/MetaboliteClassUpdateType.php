a85df0a1e9383e39323b65b334c06ae6c86ee524
Elites2017 <daveluv09@gmail.com>
2022-10-04 12:07:52 +0200

    Metabolite Class Form

---
+++ src/Form/MetaboliteClassUpdateType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\MetaboliteClass;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MetaboliteClassUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('ontology_id')
+            ->add('description', TextareaType::class, [
+                'attr' => array('cols' => '5', 'rows' => '7')])
+            ->add('parentTerm')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => MetaboliteClass::class,
+        ]);
+    }
+}
