ec266e2450a041d6c4741387fba269d699822da1
Elites2017 <daveluv09@gmail.com>
2022-10-13 15:48:22 +0200

    Set the custom entry showing to 25

--- public/assets/datatables/js/custom_page_entry_showing.js
+++ public/assets/datatables/js/custom_page_entry_showing.js
@@ -2,7 +2,7 @@ function custom_showing() {
     $(document).ready(function() {
 		$('#datatable').dataTable({
 			"aLengthMenu": [[10, 25, 50, 75, 100, -1], [10, 25, 50, 75, 100, "All"]],
-			"iDisplayLength": 50
+			"iDisplayLength": 25
 		});
 	} );
 }
