b2b70f8fa51dc14cb1bf7ba909076c6d84094aae
Elites2017 <daveluv09@gmail.com>
2022-10-13 15:41:13 +0200

    Add customed bootstrap entry showing script

---
+++ public/assets/datatables/js/custom_page_entry_showing.js
@@ -0,0 +1,10 @@
+function custom_showing() {
+    $(document).ready(function() {
+		$('#datatable').dataTable({
+			"aLengthMenu": [[10, 25, 50, 75, 100, -1], [10, 25, 50, 75, 100, "All"]],
+			"iDisplayLength": 50
+		});
+	} );
+}
+
+custom_showing();
