25c01059c3ff155e52fb82c460434ba1fb4df30f
Elites2017 <daveluv09@gmail.com>
2022-10-14 11:59:14 +0200

    Draft scale upload from file

--- src/Controller/ScaleController.php
+++ src/Controller/ScaleController.php
@@ -5,11 +5,15 @@ namespace App\Controller;
 use App\Entity\Scale;
 use App\Form\ScaleType;
 use App\Form\ScaleUpdateType;
+use App\Form\UploadFromExcelType;
 use App\Repository\ScaleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 // set a class level route
@@ -109,5 +113,147 @@ class ScaleController extends AbstractController
             'message' => $scale->getIsActive()
         ], 200);
         //return $this->redirect($this->generateUrl('season_home'));
+    }
+
+    // this is to upload data in bulk using an excel file
+    /**
+     * @Route("/upload-from-excel", name="upload_from_excel")
+     */
+    public function uploadFromExcel(Request $request, EntityManagerInterface $entmanager): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $form = $this->createForm(UploadFromExcelType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Setup repository of some entity
+            $repoScale = $entmanager->getRepository(Scale::class);
+            // Query how many rows are there in the trait table
+            $totalScaleBefore = $repoScale->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            // Return a number as response
+            // e.g 972
+
+            dd("Scale loading...");
+
+            // get the file (name from the CountryUploadFromExcelType form)
+            $file = $request->files->get('upload_from_excel')['file'];
+            // set the folder to send the file to
+            $fileFolder = __DIR__ . '/../../public/uploads/excel/';
+            // apply md5 function to generate a unique id for the file and concat it with the original file name
+            if ($file->getClientOriginalName()) {
+                $filePathName = md5(uniqid()) . $file->getClientOriginalName();
+                try {
+                    $file->move($fileFolder, $filePathName);
+                } catch (\Throwable $th) {
+                    //throw $th;
+                    $this->addFlash('danger', "Fail to upload the file, try again ");
+                }
+            } else {
+                $this->addFlash('danger', "Error in the file name, try to rename the file and try again");
+            }
+            // read from the uploaded file
+            $spreadsheet = IOFactory::load($fileFolder . $filePathName);
+            // remove the first row (title) of the file
+            $spreadsheet->getActiveSheet()->removeRow(1);
+            // transform the uploaded file to an array
+            $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+            // loop over the array to get each row
+            foreach ($sheetData as $key => $row) {
+                $ontology_id = $row['A'];
+                $name = $row['B'];
+                $description = $row['C'];
+                $chebiMass = $row['D'];
+                $chebiMonoscopic = $row['E'];
+                $synonym = $row['F'];
+                $chebiLink = $row['G'];
+                $parentTerm = $row['I'];
+                // check if the file doesn't have empty columns
+                if ($ontology_id != null && $name != null) {
+                    // check if the data is upload in the database
+                    $existingScale = $entmanager->getRepository(Scale::class)->findOneBy(['ontology_id' => $ontology_id]);
+                    // upload data only for objects that haven't been saved in the database
+                    if (!$existingScale) {
+                        $scale = new Scale();
+                        $scaleUnit = $entmanager->getRepository(Unit::class)->findOneBy(['iso3' => $UnitISO3]);
+                        if (($scaleUnit != null) && ($scaleUnit instanceof \App\Entity\Unit)) {
+                            $scale->setUnit($scaleUnit);
+                        }
+                        $scaleDataType = $entmanager->getRepository(DataType::class)->findOneBy(['iso3' => $DataTypeISO3]);
+                        if (($scaleDataType != null) && ($scaleDataType instanceof \App\Entity\DataType)) {
+                            $scale->setDataType($scaleDataType);
+                        }
+
+                        if ($this->getUser()) {
+                            $scale->setCreatedBy($this->getUser());
+                        }
+                        // $scale->setOntologyId($ontology_id);
+                        // $scale->setName($name);
+                        // if ($description != null) {
+                        //     $scale->setDescription($description);
+                        // }
+                        // if ($parentTerm != null) {
+                        //     $scale->setParOnt($parentTerm);
+                        // }
+                        // $scale->setChebiMass($chebiMass);
+                        // $scale->setChebiMonoIsoTopicMass($chebiMonoscopic);
+                        // split the text to array based on that patern
+                        $synonym = explode("|", $synonym);
+                        //var_dump($chebiMonoscopic);
+                        //$arr [] = $synonym;
+                        //dd($arr);
+                        // $scale->setSynonym($synonym);
+                        // $scale->setChebiLink($chebiLink);
+
+                        $scale->setIsActive(true);
+                        $scale->setCreatedAt(new \DateTime());
+                        $entmanager->persist($scale);
+                        $entmanager->flush();
+                    }
+                }
+            }
+            
+            $totalScaleAfter = $repoScale->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            if ($totalScaleBefore == 0) {
+                $this->addFlash('success', $totalScaleAfter . " scales have been successfuly added");
+            } else {
+                $diffBeforeAndAfter = $totalScaleAfter - $totalScaleBefore;
+                if ($diffBeforeAndAfter == 0) {
+                    $this->addFlash('success', "No new scale has been added");
+                } else if ($diffBeforeAndAfter == 1) {
+                    $this->addFlash('success', $diffBeforeAndAfter . " scale has been successfuly added");
+                } else {
+                    $this->addFlash('success', $diffBeforeAndAfter . " scales have been successfuly added");
+                }
+            }
+            return $this->redirect($this->generateUrl('scale_index'));
+        }
+
+        $context = [
+            'title' => 'Scale Upload From Excel',
+            'scaleUploadFromExcelForm' => $form->createView()
+        ];
+        return $this->render('scale/upload_from_excel.html.twig', $context);
+    }
+
+    /**
+     * @Route("/download-template", name="download_template")
+     */
+    public function excelTemplate(): Response
+    {
+        $response = new BinaryFileResponse('../public/todownload/scale_template_example.xls');
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'scale_template_example.xls');
+        return $response;
+       
     }
 }
