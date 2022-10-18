3e4059f0bf7147f589ba9bc6c79aadf1b4831316
Elites2017 <daveluv09@gmail.com>
2022-10-18 11:52:14 +0200

    Add html help to create new object

--- src/Form/AttributeTraitValueType.php
+++ src/Form/AttributeTraitValueType.php
@@ -2,17 +2,34 @@
 
 namespace App\Form;
 
+use App\Entity\Accession;
+use App\Entity\Attribute;
 use App\Entity\AttributeTraitValue;
+use App\Entity\MetabolicTrait;
+use App\Entity\TraitClass;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\RouterInterface;
 
 class AttributeTraitValueType extends AbstractType
 {
+    private $router;
+
+    function __construct(RouterInterface $router){
+        $this->router = $router;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $toUrlTrait = $this->router->generate('trait_class_create');
+        $toUrlMetabolicTrait = $this->router->generate('metabolic_trait_create');
+        $toUrlAttribute = $this->router->generate('attribute_create');
+        $toUrlAccession = $this->router->generate('accession_create');
+
         $builder
             ->add('value')
             ->add('publicationReference', CollectionType::class, [
@@ -22,10 +39,34 @@ class AttributeTraitValueType extends AbstractType
                 'label' => false,
                 'prototype_data' => 'Publication reference...'
             ])
-            ->add('trait')
-            ->add('metabolicTrait')
-            ->add('attribute')
-            ->add('accession')
+            ->add('trait', EntityType::class, [
+                'class' => TraitClass::class,
+                'help_html' => true,
+                'placeholder' => '',
+                'help' => 'Add a new <a href="' . $toUrlTrait .'" target="_blank">Trait</a>'
+                
+            ])
+            ->add('metabolicTrait', EntityType::class, [
+                'class' => MetabolicTrait::class,
+                'help_html' => true,
+                'placeholder' => '',
+                'help' => 'Add a new <a href="' . $toUrlMetabolicTrait .'" target="_blank">Metabolic Trait</a>'
+                
+            ])
+            ->add('attribute', EntityType::class, [
+                'class' => Attribute::class,
+                'help_html' => true,
+                'placeholder' => '',
+                'help' => 'Add a new <a href="' . $toUrlAttribute .'" target="_blank">Attribute</a>'
+                
+            ])
+            ->add('accession', EntityType::class, [
+                'class' => Accession::class,
+                'help_html' => true,
+                'placeholder' => '',
+                'help' => 'Add a new <a href="' . $toUrlAccession .'" target="_blank">Accession</a>'
+                
+            ])
         ;
     }
 
