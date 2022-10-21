18e7187f0fb077c5f84ddee524773730e4d2a55c
Elites2017 <daveluv09@gmail.com>
2022-10-21 15:47:31 +0200

    Migration to add abbreviation in location

---
+++ migrations/Version20221021125422.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221021125422 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE location ADD abbreviation VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_5E9E89CBBCF3411D ON location (abbreviation)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_5E9E89CBBCF3411D ON location');
+        $this->addSql('ALTER TABLE location DROP abbreviation');
+    }
+}
