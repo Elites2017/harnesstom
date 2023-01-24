5476c591ea075cdf7e8002f371ba2746e198f31f
Elites2017 <daveluv09@gmail.com>
2023-01-24 15:54:23 +0100

    Add datalist for institute country - To be fixed id shown in the value

---
+++ src/Form/DatalistType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DatalistType extends AbstractType
+{
+    public function getParent()
+    {
+        return EntityType::class;
+    }
+}
