ef4ef4a734ef109db1b56e75b6bc0767d00dd0e8
Elites2017 <daveluv09@gmail.com>
2023-04-13 10:02:32 +0200

    Add marker synonym upload from file

--- src/Controller/MarkerSynonymController.php
+++ src/Controller/MarkerSynonymController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\MarkerSynonym;
+use App\Entity\Marker;
 use App\Form\MarkerSynonymType;
 use App\Form\MarkerSynonymUpdateType;
+use App\Form\UploadFromExcelType;
 use App\Repository\MarkerSynonymRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -114,7 +117,119 @@ class MarkerSynonymController extends AbstractController
         //return $this->redirect($this->generateUrl('season_home'));
     }
 
-    
+    // this is to upload data in bulk using an excel file
+    /**
+     * @Route("/upload-from-excel", name="upload_from_excel")
+     */
+    public function uploadFromExcel(Request $request, EntityManagerInterface $entmanager): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $form = $this->createForm(UploadFromExcelType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Setup repository of some entity
+            $repoMarkerSynonym = $entmanager->getRepository(MarkerSynonym::class);
+            // Query how many rows are there in the Marker table
+            $totalMarkerSynonymBefore = $repoMarkerSynonym->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            // Return a number as response
+            // e.g 972
+
+            // get the file (name from the CountryUploadFromExcelType form)
+            $file = $request->files->get('upload_from_excel')['file'];
+            // set the folder to send the file to
+            $fileFolder = __DIR__ . '/../../public/uploads/excel/';
+            // apply md5 function to generate a unique id for the file and concat it with the original file name
+            if ($file->getClientOriginalName()) {
+                $filePathName = md5(uniqid()) . $file->getClientOriginalName();
+                try {
+                    $file->move($fileFolder, $filePathName);
+                } catch (\Throwable $th) {
+                    //throw $th;
+                    $this->addFlash('danger', "Fail to upload the file, try again");
+                }
+            } else {
+                $this->addFlash('danger', "Error in the file name, try to rename the file and try again");
+            }
+            // read from the uploaded file
+            $spreadsheet = IOFactory::load($fileFolder . $filePathName);
+            // remove the first row (title) of the file
+            $spreadsheet->getActiveSheet()->removeRow(1);
+            // transform the uploaded file to an array
+            $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+            // loop over the array to get each row
+            foreach ($sheetData as $key => $row) {
+                $markerName = $row['A'];
+                $synonymSource = $row['B'];
+                $markerSynonymId = $row['C'];
+                // check if the file doesn't have empty columns
+                if ($markerName != null & $synonymSource != null && $markerSynonymId != null) {
+                    // check if for the provided name, a maker exists in the DB
+                    $existingMarker = $entmanager->getRepository(Marker::class)->findOneBy(['name' => $markerName]);
+                    if ($existingMarker) {
+                        // check if the data is upload in the database
+                        $existingMarkerSynonym = $entmanager->getRepository(MarkerSynonym::class)->findOneBy(['markerNameId' => $existingMarker->getId(), 'synonymSource' => $markerSynonym, 'markerSynonymId' => $markerSynonymId]);
+                        // upload data only for objects that haven't been saved in the database
+                        if (!$existingMarkerSynonym) {
+                            $markerSynonym = new MarkerSynonym();
+                            $markerSynonym->setMarkerNameId($existingMarker->getId());
+                            $markerSynonym->setSynonymSource($synonymSource);
+                            $markerSynonym->setSynonymSource($markerSynonymId);
+                            if ($this->getUser()) {
+                                $marker->setCreatedBy($this->getUser());
+                            }
+                            $marker->setIsActive(true);
+                            $marker->setCreatedAt(new \DateTime());
+                            
+                            try {
+                                //code...
+                                $entmanager->persist($marker);
+                                $entmanager->flush();
+                            } catch (\Throwable $th) {
+                                //throw $th;
+                                $this->addFlash('danger', "A problem happened, we can not save your data now due to: " .strtoupper($th->getMessage()));
+                            }
+                        }
+                    } else {
+                        $this->addFlash('danger', "The marker name " .$markerName. " doesn't exist in our database.");
+                    }
+                }
+            }
+            
+            // Query how many rows are there in the Country table
+            $totalMarkerSynonymAfter = $repoMarkerSynonym->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            if ($totalMarkerSynonymBefore == 0) {
+                $this->addFlash('success', $totalMarkerSynonymAfter . " Marker synonyms have been successfuly added");
+            } else {
+                $diffBeforeAndAfter = $totalMarkerSynonymAfter - $totalMarkerSynonymBefore;
+                if ($diffBeforeAndAfter == 0) {
+                    $this->addFlash('success', "No new marker synonym has been added");
+                } else if ($diffBeforeAndAfter == 1) {
+                    $this->addFlash('success', $diffBeforeAndAfter . " Marker synonym has been successfuly added");
+                } else {
+                    $this->addFlash('success', $diffBeforeAndAfter . " Marker synonyms have been successfuly added");
+                }
+            }
+            return $this->redirect($this->generateUrl('marker_synonym_index'));
+        }
+
+        $context = [
+            'title' => 'Marker Synonym Upload From Excel',
+            'markerSynonymUploadFromExcelForm' => $form->createView()
+        ];
+        return $this->render('marker_synonym/upload_from_excel.html.twig', $context);
+    }
 
     /**
      * @Route("/download-template", name="download_template")
@@ -123,7 +238,6 @@ class MarkerSynonymController extends AbstractController
     {
         $response = new BinaryFileResponse('../public/todownload/marker_synonym_template_example.xlsx');
         $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'marker_synonym_template_example.xlsx');
-        return $response;
-       
+        return $response;  
     }
 }
