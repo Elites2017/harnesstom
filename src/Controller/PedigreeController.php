d42c417377a12e17becf0f8a6276a32cd63f81f3
Elites2017 <daveluv09@gmail.com>
2023-05-31 11:55:01 +0200

    Add pedigree upload from excel - TBF the relationshp MTM

--- src/Controller/PedigreeController.php
+++ src/Controller/PedigreeController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Cross;
+use App\Entity\Generation;
 use App\Entity\Pedigree;
 use App\Entity\Progeny;
 use App\Form\PedigreeType;
 use App\Form\PedigreeUpdateType;
+use App\Form\UploadFromExcelType;
 use App\Repository\PedigreeRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 // set a class level route
@@ -144,5 +150,177 @@ class PedigreeController extends AbstractController
             'code' => 200,
             'message' => $pedigree->getIsActive()
         ], 200);
+    }
+
+    // this is to upload data in bulk using an excel file
+    /**
+     * @Route("/upload-from-excel", name="upload_from_excel")
+     */
+    public function uploadFromExcel(Request $request, EntityManagerInterface $entmanager): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $form = $this->createForm(UploadFromExcelType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Setup repository of some entity
+            $repoPedigree = $entmanager->getRepository(Pedigree::class);
+            // Query how many rows are there in the Pedigree table
+            $totalPedigreeBefore = $repoPedigree->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            // Return a number as response
+            // e.g 972
+
+            // get the file (name from the CountryUploadFromExcelType form)
+            $file = $request->files->get('upload_from_excel')['file'];
+            // set the folder to send the file to
+            $fileFolder = __DIR__ . '/../../public/uploads/excel/';
+            // apply md5 function to generate a unique id for the file and concat it with the original file name
+            if ($file->getClientOriginalName()) {
+                $filePathName = md5(uniqid()) . $file->getClientOriginalName();
+                try {
+                    $file->move($fileFolder, $filePathName);
+                } catch (\Throwable $th) {
+                    //throw $th;
+                    $this->addFlash('danger', "Fail to upload the file, try again ");
+                }
+            } else {
+                $this->addFlash('danger', "Error in the file name, try to rename the file and try again");
+            }
+
+            // read from the uploaded file
+            $spreadsheet = IOFactory::load($fileFolder . $filePathName);
+            // remove the first row (title) of the file
+            $spreadsheet->getActiveSheet()->removeRow(1);
+            // transform the uploaded file to an array
+            $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+            // loop over the array to get each row
+            foreach ($sheetData as $key => $row) {
+                $pedigreeEntryId = $row['A'];
+                $germplasmDbId = $row['B'];
+                $crossName = $row['C'];
+                $generationOntId = $row['D'];
+                $ancestorPedigreeId = $row['E'];
+                // check if the file doesn't have empty columns
+                if ($pedigreeEntryId != null && $germplasmDbId != null && $crossName != null && $generationOntId != null) {
+                    // check if the data is upload in the database
+                    $existingPedigree = $entmanager->getRepository(Pedigree::class)->findOneBy(['pedigreeEntryID' => $pedigreeEntryId]);
+                    // upload data only for objects that haven't been saved in the database
+                    if (!$existingPedigree) {
+                        $pedigree = new Pedigree();
+                        if ($this->getUser()) {
+                            $pedigree->setCreatedBy($this->getUser());
+                        }
+
+                        try {
+                            //code...
+                            $pedigree->setPedigreeEntryID($pedigreeEntryId);
+                        } catch (\Throwable $th) {
+                            //throw $th;
+                            $this->addFlash('danger', " there is a problem with the pedigree entry ID " .$pedigreeEntryId);
+                        }
+
+                        if ($ancestorPedigreeId) {
+                            try {
+                                //code...
+                                $pedigree->setAncestorPedigreeEntryID($ancestorPedigreeId);
+                            } catch (\Throwable $th) {
+                                //throw $th;
+                                $this->addFlash('danger', " there is a problem with the ancesstor pedigree ID " .$ancestorPedigreeId);
+                            }   
+                        }
+
+                        try {
+                            //code...
+                            $pedigreeCross = $entmanager->getRepository(Cross::class)->findOneBy(['name' => $crossName]);
+                            if (($pedigreeCross != null) && ($pedigreeCross instanceof \App\Entity\Cross)) {
+                                $pedigree->setPedigreeCross($pedigreeCross);
+                            }
+                        } catch (\Throwable $th) {
+                            //throw $th;
+                            $this->addFlash('danger', " there is a problem with the cross name " .$crossName);
+                        }
+
+                        try {
+                            //code...
+                            $pedigreeGeneration = $entmanager->getRepository(Generation::class)->findOneBy(['ontology_id' => $generationOntId]);
+                            if (($pedigreeGeneration != null) && ($pedigreeGeneration instanceof \App\Entity\Generation)) {
+                                $pedigree->setGeneration($pedigreeGeneration);
+                            }
+                        } catch (\Throwable $th) {
+                            //throw $th;
+                            $this->addFlash('danger', " there is a problem with the generation ontology ID " .$generationOntId);
+                        }
+
+                        try {
+                            //code...
+                            $pedigreeGermplasm = $entmanager->getRepository(Germplasm::class)->findOneBy(['germplasmID' => $germplasmDbId]);
+                            if (($pedigreeGermplasm != null) && ($pedigreeGermplasm instanceof \App\Entity\Germplasm)) {
+                                $pedigree->addGermplasm($pedigreeGermplasm);
+                            }
+                        } catch (\Throwable $th) {
+                            //throw $th;
+                            $this->addFlash('danger', " there is a problem with the germplasm " .$germplasmDbId);
+                        }
+
+                        $pedigree->setIsActive(true);
+                        $pedigree->setCreatedAt(new \DateTime());
+                        try {
+                            //code...
+                            $entmanager->persist($pedigree);
+                            $entmanager->flush();
+                        } catch (\Throwable $th) {
+                            //throw $th;
+                            $this->addFlash('danger', "A problem happened, we can not save your data now due to: " .strtoupper($th->getMessage()));
+                        }
+                    }
+                } else {
+                    $this->addFlash('danger', " The pedigree entry ID, the cross name, the germplasm ID and the generation ontology ID can not be empty, provide them and try again");
+                }
+            }
+            
+            // Query how many rows are there in the table
+            $totalPedigreeAfter = $repoPedigree->createQueryBuilder('tab')
+                // Filter by some parameter if you want
+                // ->where('a.isActive = 1')
+                ->select('count(tab.id)')
+                ->getQuery()
+                ->getSingleScalarResult();
+
+            if ($totalPedigreeBefore == 0) {
+                $this->addFlash('success', $totalPedigreeAfter . " pedigrees have been successfuly added");
+            } else {
+                $diffBeforeAndAfter = $totalPedigreeAfter - $totalPedigreeBefore;
+                if ($diffBeforeAndAfter == 0) {
+                    $this->addFlash('success', "No new pedigree has been added");
+                } else if ($diffBeforeAndAfter == 1) {
+                    $this->addFlash('success', $diffBeforeAndAfter . " pedigree has been successfuly added");
+                } else {
+                    $this->addFlash('success', $diffBeforeAndAfter . " pedigrees have been successfuly added");
+                }
+            }
+            return $this->redirect($this->generateUrl('pedigree_index'));
+        }
+
+        $context = [
+            'title' => 'Pedigree Upload From Excel',
+            'pedigreeUploadFromExcelForm' => $form->createView()
+        ];
+        return $this->render('pedigree/upload_from_excel.html.twig', $context);
+    }
+
+    /**
+     * @Route("/download-template", name="download_template")
+     */
+    public function excelTemplate(): Response
+    {
+        $response = new BinaryFileResponse('../public/todownload/pedigree_template_example.xlsx');
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'pedigree_template_example.xlsx');
+        return $response;
+       
     }
 }
