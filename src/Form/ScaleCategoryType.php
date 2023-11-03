afde27cbe8a30822cdd5ac81295aa19b542d7b1b
Elites2017 <daveluv09@gmail.com>
2023-11-03 10:56:17 +0100

    Make scale mandatory

--- src/Form/ScaleCategoryType.php
+++ src/Form/ScaleCategoryType.php
@@ -2,21 +2,38 @@
 
 namespace App\Form;
 
+use App\Entity\Scale;
 use App\Entity\ScaleCategory;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\RouterInterface;
 
 class ScaleCategoryType extends AbstractType
 {
+    private $router;
+
+    function __construct(RouterInterface $router){
+        $this->router = $router;
+    }
+    
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $toUrlScale = $this->router->generate('scale_create');
+
         $builder
             ->add('label')
             ->add('score')
             ->add('min')
             ->add('max')
-            ->add('scale')
+            ->add('scale', EntityType::class, [
+                'class' => Scale::class,
+                'help_html' => true,
+                'placeholder' => '',
+                'help' => 'Add a new <a href="' . $toUrlScale .'" target="_blank">Scale</a>'
+                
+            ])
         ;
     }
 
