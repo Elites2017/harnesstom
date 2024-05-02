b92cc2667fc78f1ee5c8af951a17b4c006587908
Elites2017 <daveluv09@gmail.com>
2024-05-02 14:29:39 +0200

    Fix accession datalist edit

---
+++ src/Form/Datalist10Type.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+
+class Datalist10Type extends AbstractType
+{
+    public function getParent()
+    {
+        return EntityType::class;
+    }
+}
