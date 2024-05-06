ac75647af732236da1cb045b291ab723f338683c
Elites2017 <daveluv09@gmail.com>
2024-05-06 11:05:42 +0200

    Add create & edit qtl epistatis effect datalist

--- src/Form/QTLEpistasisEffectType.php
+++ src/Form/QTLEpistasisEffectType.php
@@ -3,21 +3,47 @@
 namespace App\Form;
 
 use App\Entity\QTLEpistasisEffect;
+use App\Entity\QTLVariant;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\RouterInterface;
 
 class QTLEpistasisEffectType extends AbstractType
 {
+    private $router;
+
+    function __construct(RouterInterface $router){
+        $this->router = $router;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $toUrlQtlVariant = $this->router->generate('qtl_variant_create');
+
         $builder
             ->add('addEpi')
             ->add('r2Add')
             ->add('r2Epi')
             ->add('epistatisticEpi')
-            ->add('qtlVariant1')
-            ->add('qtlVariant2')
+            ->add('qtlVariant1', DatalistType::class, [
+                'class' => QTLVariant::class,
+                'help_html' => true,
+                'required' => true,
+                'placeholder' => '',
+                'choice_value' => 'name',
+                'help' => 'Add a new <a href="' . $toUrlQtlVariant .'" target="_blank">QTL Variant</a>'
+                
+            ])
+            ->add('qtlVariant2', Datalist1Type::class, [
+                'class' => QTLVariant::class,
+                'help_html' => true,
+                'required' => true,
+                'placeholder' => '',
+                'choice_value' => 'name',
+                'help' => 'Add a new <a href="' . $toUrlQtlVariant .'" target="_blank">QTL Variant</a>'
+                
+            ])
             ->add('epistatisticAdd')
         ;
     }
