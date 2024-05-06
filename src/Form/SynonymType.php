b3d29cc49c397e7239faee12dbfc682000f24c58
Elites2017 <daveluv09@gmail.com>
2024-05-06 13:16:00 +0200

    Add synonym create & edit datalist

--- src/Form/SynonymType.php
+++ src/Form/SynonymType.php
@@ -2,19 +2,35 @@
 
 namespace App\Form;
 
+use App\Entity\Accession;
 use App\Entity\Synonym;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\RouterInterface;
 
 class SynonymType extends AbstractType
 {
+    private $router;
+
+    function __construct(RouterInterface $router){
+        $this->router = $router;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $toUrlAccession = $this->router->generate('accession_create');
+
         $builder
             ->add('synonymSource')
             ->add('synonymId')
-            ->add('accession')
+            ->add('accession', DatalistType::class, [
+                'class' => Accession::class,
+                'help_html' => true,
+                'placeholder' => '',
+                'choice_value' => 'maintainerNumb',
+                'help' => 'Add a new <a href="' . $toUrlAccession .'" target="_blank">Accession</a>'  
+            ])
         ;
     }
 
