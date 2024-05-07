61774cac711559ae5c04dd390a9d475ce6d0e54d
Elites2017 <daveluv09@gmail.com>
2024-05-07 13:38:34 +0200

    Add method class create & edit datalist

--- src/Form/MethodClassType.php
+++ src/Form/MethodClassType.php
@@ -17,7 +17,12 @@ class MethodClassType extends AbstractType
             ->add('ontology_id')
             ->add('description', TextareaType::class, [
                 'attr' => array('cols' => '5', 'rows' => '7')])
-            ->add('parentTerm')
+            ->add('parentTerm', DatalistType::class, [
+                'class' => MethodClass::class,
+                'help_html' => true,
+                'placeholder' => '',
+                'choice_value' => 'name',  
+            ])
         ;
     }
 
