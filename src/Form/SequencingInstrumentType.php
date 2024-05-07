2fed892c41839e227807fbe29ac1fc1d6e83b162
Elites2017 <daveluv09@gmail.com>
2024-05-07 10:51:32 +0200

    Add sequencing instrument create & edit datalist

--- src/Form/SequencingInstrumentType.php
+++ src/Form/SequencingInstrumentType.php
@@ -17,7 +17,12 @@ class SequencingInstrumentType extends AbstractType
             ->add('ontology_id')
             ->add('description', TextareaType::class, [
                 'attr' => array('cols' => '5', 'rows' => '7')])
-            ->add('parentTerm')
+            ->add('parentTerm', DatalistType::class, [
+                'class' => SequencingInstrument::class,
+                'help_html' => true,
+                'placeholder' => '',
+                'choice_value' => 'name',  
+            ])
         ;
     }
 
